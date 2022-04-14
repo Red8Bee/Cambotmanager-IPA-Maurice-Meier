29c9096a364670ee1ce80760dbc3a4e2137dd9f1
maurice.meier <maurice.meier@fhnw.ch>
2022-04-14 15:12:03 +0200

    wrong cam port

--- cambotmanager/Robot/cam_nopyrealsense.py
+++ cambotmanager/Robot/cam_nopyrealsense.py
@@ -5,12 +5,14 @@ from datetime import datetime
 
 # this takes pictures with the OpenCV library
 def take_images(snapshot_parent_inventory_item):
-    cam_port = 5  # the Camera with shel be used if images look strange swap number start with 0
+    cam_port = 4  # the Camera with shel be used if images look strange swap number start with 0
     cam = cv2.VideoCapture(cam_port)
     path = snapshot_parent_inventory_item.base_directory + '/' + snapshot_parent_inventory_item.image_directory + '/'
     name = 'snapshot_RGB_' + datetime.now().strftime('%Y%m%d%H%M%S') + '.jpg'
     whole_path = path + name
     s, img = cam.read()
     if s:
+        time.sleep(1)
         cv2.imwrite(whole_path, img)
+        time.sleep(1)
         return whole_path
